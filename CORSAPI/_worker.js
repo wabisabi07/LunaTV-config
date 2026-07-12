f82ef46ae685e85d6fad2cfddd79410dcc50bac3
wabisabi07 <124358979+wabisabi07@users.noreply.github.com>
2026-07-12 23:17:59 +0800

    Update full JSON source URL in _worker.js

--- CORSAPI/_worker.js
+++ CORSAPI/_worker.js
@@ -26,7 +26,7 @@ const EXCLUDE_HEADERS = new Set([
 const JSON_SOURCES = {
   'jin18': 'https://raw.githubusercontent.com/hafrey1/LunaTV-config/refs/heads/main/jin18.json',
   'jingjian': 'https://raw.githubusercontent.com/hafrey1/LunaTV-config/refs/heads/main/jingjian.json',
-  'full': 'https://raw.githubusercontent.com/hafrey1/LunaTV-config/refs/heads/main/LunaTV-config.json'
+  'full': 'https://raw.githubusercontent.com/wabisabi07/LunaTV-config/refs/heads/main/LunaTV-config.json'
 }
 
 const FORMAT_CONFIG = {
